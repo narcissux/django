9774ae7d9c398574be2b5400aa74ecfa2280461e
narcissux <fengxingjimo@163.com>
2018-11-18 09:44:49 +0800

    提交修改nginx的静态目录位置

--- Django/settings.py
+++ Django/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT =os.path.join(BASE_DIR, "static")
+'''
 STATICFILES_DIRS = (
 #static前面不能加斜杠
 os.path.join(BASE_DIR, "static"),
 )
+'''
+
+
