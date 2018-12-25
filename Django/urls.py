2099a3d27a00033e1ea235738c3db96f5d98bcc1
narcissux <fengxingjimo@163.com>
2018-12-26 00:08:00 +0800

    提交github 的webhook自动更新代码功能

--- Django/urls.py
+++ Django/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import views
 
 #网址和模板的映射
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls', namespace='blog')),
+    path('github/',views.handle_github_hook),
 ]
---
+++ Django/views.py
@@ -0,0 +1,20 @@
+import hmac
+from git import Repo
+from django.http import HttpResponse
+
+
+# github的webhook请求处理
+def handle_github_hook(request):
+    signature = request.META['HTTP_X_HUB_SIGNATURE']
+    if signature.find('=')>=0:
+        sha, signature = signature.split('=')
+    secret = str.encode("galgameplay")
+    hashhex = hmac.new(secret, request.body, digestmod='sha1').hexdigest()
+    if hmac.compare_digest(hashhex, signature):
+        repo = Repo('/home/gwt/sites/docker.narcissu.tk/django')
+        origin = repo.remotes.origin
+        origin.pull('--rebase')
+        commit = request.json['after'][0:6]
+        print('Repository updated with commit {}'.format(commit))
+        return HttpResponse('', status=200)
+    return HttpResponse('', status=400)
